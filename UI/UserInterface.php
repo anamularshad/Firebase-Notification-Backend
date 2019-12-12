14e0101a42856adc5b1b071f3875b218f808cf9f
Shuvo <shovocse753@gmail.com>
2019-12-12 23:16:26 +0600

    UserInterface.php and style.css is created.

---
+++ UI/UserInterface.php
@@ -0,0 +1,24 @@
+<?php  ?>
+<html>
+  <head>
+    <title>Firebase Notification</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style/style.css">
+  </head>
+
+
+  <body>
+    <form>
+      <fieldset>
+        <legend>Notification::</legend>
+        <label>Title</label></br>
+        <input type="text" name="title" class="pure-input-1-2" placeholder="Notification title"></br></br>
+        <label>Message</label></br>
+        <input type="text" name="message" class="pure-input-1-2" placeholder="Notification message"></br></br>
+        <button type="submit" class="pure-button pure-button-primary send_button">Send</button>
+      </fieldset>
+    </form>
+  </body>
+
+</html>
